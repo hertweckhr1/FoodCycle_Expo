0f0787e3e417a7f42a23f1ac72572db02406f132
Hannah <hannahvanseeters@gmail.com>
2019-01-18 11:46:42 -0800

    maps added with Seattle lat long in near you tab

--- src/screens/FindNearYou.js
+++ src/screens/FindNearYou.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import { MapView } from 'expo';
+
 
 class FindNearYou extends Component {
   render() {
     return (
-      <Text>Find Near You</Text>
+        <MapView
+          style={{ width: 600, height: 400 }}
+          initialRegion={{
+            latitude: 47.608013,
+            longitude: -122.335167,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
     )
   }
 }
