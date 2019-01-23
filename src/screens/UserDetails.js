93e97a0db7cb9b0eeb0e1d68af30c8ba4e165616
Hannah <hannahvanseeters@gmail.com>
2019-01-23 15:27:57 -0800

    changed some margins on userdetails

--- src/screens/UserDetails.js
+++ src/screens/UserDetails.js
@@ -31,29 +31,29 @@ class UserDetails extends React.Component {
       <Container>
           <Grid>
             <Row>
-              <Col style={{ backgroundColor: '#FF4500', height: 200,
+              <Col style={{ backgroundColor: '#FF4500', height: 180,
                 margin: 10, marginRight: 7,  borderRadius: 8}}>
-                <Text style={styles.containerHeaderText}>Donations Offered Today</Text>
+                <Text style={styles.containerHeaderText}>Donations Today</Text>
                 <Text style={styles.numberText}>{listLength(donationsToday)}</Text>
               </Col>
-              <Col style={{ backgroundColor: '#9ACD32', height: 200,
+              <Col style={{ backgroundColor: '#9ACD32', height: 180,
                 margin: 10, marginLeft: 7, borderRadius: 8 }}>
                 <Text style={styles.containerHeaderText}>Pickups Scheduled</Text>
                 <Text style={styles.numberText}>{listLength(pickedupDonations)}</Text>
               </Col>
             </Row>
             <Row style={{marginTop: 10}}>
-              <Col style={{ backgroundColor: '#339933', height: 200,
-                marginTop: 20, marginLeft: 10, marginRight: 10,  borderRadius: 8  }}>
+              <Col style={{ backgroundColor: '#339933', height: 190,
+                marginLeft: 10, marginRight: 10,  borderRadius: 8  }}>
                 <Text style={styles.containerHeaderText}>Total Donations Contributed</Text>
                 <Text style={styles.numberText}>{listLength(filteredDonations)}</Text>
               </Col>
             </Row>
-            <Row style={{marginTop: 20}}>
-              <Col style={{ backgroundColor: 'tomato', height: 180,
-                marginTop: 20, marginLeft: 10, marginRight: 10, borderRadius: 8 }}>
+            <Row style={{marginTop: 10}}>
+              <Col style={{ backgroundColor: 'tomato', height: 190,
+                 marginLeft: 10, marginRight: 10, borderRadius: 8 }}>
                 <Text style={styles.containerHeaderText}>Meals Donated Equivalent</Text>
-                <Text style={styles.numberText}>103</Text>
+                <Text style={styles.numberText}>{donations.length}</Text>
               </Col>
             </Row>
           </Grid>
