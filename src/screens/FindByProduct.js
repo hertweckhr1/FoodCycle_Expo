193d36fe4a1f0dc54f4d3b6341733f9be5df18e9
Hannah <hannahvanseeters@gmail.com>
2019-01-22 19:24:34 -0800

    search bar is working in basic sense for all donations

--- src/screens/FindByProduct.js
+++ src/screens/FindByProduct.js
@@ -1,119 +1,79 @@
-import React, {Component} from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native' ;
-import { H1, Container, Button, Thumbnail, Body, Left, Right, Content, List, ListItem, Text } from 'native-base';
-import moment from "moment";
-
+import React, { Component } from 'react';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { List, ListItem, SearchBar } from 'react-native-elements';
 
 class FindByProduct extends Component {
-  render () {
-    const { user, donations, users } = this.props.screenProps
-    const today = moment(new Date()).format("YYYY-MM-DD")
-    const donationsToday = donations.filter(donation =>
-      moment(donation['pickup_starttime']).format("YYYY-MM-DD") == today);
-    const donationsAvailableToday = donationsToday.filter(donation =>
-      donation['status'] == 'posted')
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // loading: false,
+      data: this.props.screenProps.donations,
+      error: null,
+    };
+
+    this.arrayholder = this.props.screenProps.donations;
+  }
 
 
-    const thisUser = (id) => {
-      if (id == null) {
-        return ""
-      } else {
-        const rightUser = users.find(user => user['id'] == id)['company_name'];
-        return rightUser
-      }
-    }
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '14%',
+        }}
+      />
+    );
+  };
 
-    if (donationsAvailableToday.length == 0) {
-      return (
-        <Container>
-          <View style={styles.titleView}>
-            <H1 style={styles.headerText}>Donations Available Today</H1>
-          </View>
-          <View>
-            <Text style={styles.noticeText}>There are currently no available donations at this location.</Text>
-          </View>
-        </Container>
-      )
-    } else {
-      return (
-        <ScrollView>
-          <Container>
-            <Content>
-              <View style={styles.titleView}>
-                <H1 style={styles.headerText}>Donations Available Today</H1>
-                <Text note style={styles.subTitle}>Total Donations: {donationsAvailableToday.length}</Text>
-              </View>
-              <List dataArray={donationsAvailableToday}
-               renderRow={(donation) =>
-                 <ListItem thumbnail>
+  searchFilterFunction = text => {
+    console.log(this.arrayholder);
+    const newData = this.arrayholder.filter(item => {
+      const itemData = `${item['product_description'].toUpperCase()} ${item['product_type'].toUpperCase()}`;
+      console.log(itemData)
+      const textData = text.toUpperCase();
+      console.log(textData)
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      data: newData,
+    });
+  };
 
-                   <Body>
-                     <Text style={styles.text}>{donation['product_description']}</Text>
-                     <Text note style={styles.text}>Type: {donation['product_type']}</Text>
-                     <Text note style={styles.text}>Donor: {thisUser(donation['user'])}</Text>
-                     <Text style={styles.text} note numberOfLines={1}>Pick Up: {moment(donation['pickup_starttime']).format("hh:mm a")} to {moment(donation['pickup_endtime']).format("hh:mm a")}</Text>
-                   </Body>
-                   <Right>
-                     <View style={{flexDirection: 'column'}}>
-                       <Button style={styles.button}
-                         onPress={() => this.props.screenProps.updateDonationCallback(user['id'], donation['id'])}>
-                         <Text style={styles.buttonText}>Pick Up</Text>
-                       </Button>
-                       <Button style={styles.button}
-                         onPress={() => this.props.navigation.navigate('DoneeDonationDetail', {donationID: donation['id']})}>
-                         <Text style={styles.buttonText}>See Details</Text>
-                       </Button>
-                     </View>
-                   </Right>
-                 </ListItem>
-               }>
-              </List>
-            </Content>
-          </Container>
-        </ScrollView>
-      );
-    }
+  renderHeader = () => {
+    return (
+      <SearchBar
+        placeholder="Type Here..."
+        lightTheme
+        round
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+      />
+    );
+  };
+
+  render() {
+    return (
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              roundAvatar
+              title={`${item['product_description']}: ${item['product_type']}`}
+              subtitle={item['product_measurement']}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />
+          )}
+          KeyExtractor={item => item['id']}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+        />
+      </List>
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  headerText: {
-    textAlign: 'center',
-    fontFamily: 'Futura',
-    fontWeight: 'bold',
-    paddingTop: 20,
-    paddingBottom: 10,
-  },
-  titleView: {
-    borderBottomWidth: 0.5,
-    borderBottomColor: '#D3D3D3',
-    margin: 10,
-  },
-  noticeText: {
-    textAlign: 'center',
-    fontFamily: 'Futura',
-    paddingTop: 20,
-    paddingBottom: 10,
-    color: '#FF4500',
-  },
-  button: {
-    backgroundColor: 'tomato',
-    marginBottom: 7,
-    justifyContent: 'center'
-  },
-  text: {
-    fontFamily: 'Futura',
-  },
-  buttonText: {
-    fontFamily: 'Futura',
-    color: 'white',
-    fontSize: 12,
-  },
-  subTitle: {
-    fontFamily: 'Futura',
-    textAlign: 'center',
-  }
-
-})
-
 export { FindByProduct };
